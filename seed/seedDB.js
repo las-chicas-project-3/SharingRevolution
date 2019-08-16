030d6b8cca2c461e76eabbcba20c66edbc9e1820
Mafalda <mafalda.villazan@gmail.com>
2019-08-16 13:21:40 -0700

    Creating the seed for the books

--- seed/seedDB.js
+++ seed/seedDB.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
-
 mongoose.connect(
   process.env.MONGODB_URI ||
   "mongodb://localhost/SharingRevolution"
 );
 
+
+//Client
 const clientSeed = [
   {
     name: "Joyce",
@@ -34,6 +34,22 @@ const clientSeed = [
   }
 ];
 
+//Object
+const objectSeed = [
+  {
+    name: "Reusable Cup",
+    points: 10,
+    description: "Reuse it on Starbucks"
+  },
+  {
+    name: "Iphone charger",
+    points: 10,
+    description: "When running out of battery is there to help"
+  }
+];
+
+
+
 db.Client
   .remove({})
   .then(() => db.Client.collection.insertMany(clientSeed))
@@ -45,3 +61,18 @@ db.Client
     console.error(err);
     process.exit(1);
   });
+
+
+db.Client
+  .remove({})
+  .then(() => db.Object.collection.insertMany(objectSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+
