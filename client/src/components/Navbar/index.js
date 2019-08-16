232dd4c1bd9ae51c2367ff8f0cc8ebfc48d77ca0
Caroline Kalorkoti <carolineecrandell@gmail.com>
2019-08-16 11:56:56 -0700

    Navbar edits

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,36 +7,35 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Pupster
+        Home
       </Link>
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
+              to="/products"
+              className={window.location.pathname === "/products"
                   ? "nav-link active"
                   : "nav-link"
               }
             >
-              About
+              Products
             </Link>
           </li>
           <li className="nav-item">
             <Link
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+              to="/signup"
+              className={window.location.pathname === "/signup" ? "nav-link active" : "nav-link"}
             >
-              Discover
+              Signup
             </Link>
           </li>
           <li className="nav-item">
             <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+              to="/login"
+              className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}
             >
-              Search
+              Login
             </Link>
           </li>
         </ul>
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 // import Foundation from 'react-foundation';
-import 'foundation-sites/dist/css/foundation.min.css';
+// import 'foundation-sites/dist/css/foundation.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
