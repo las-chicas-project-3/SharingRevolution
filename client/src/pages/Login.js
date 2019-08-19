bb0112f39633b0bf32004934b6c009c275d90ab9
Mafalda <mafalda.villazan@gmail.com>
2019-08-19 10:40:49 -0700

    Adding the validation functions

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,32 @@ import React, { Component } from "react";
 import Container from "../components/Container";
 import LoginForm from "../components/LoginForm";
 
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+  // Convert empty fields to an empty string so we can use validator functions
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  // Email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  // Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
+
+
+
 class Login extends Component {
 
   render() {
@@ -9,7 +35,7 @@ class Login extends Component {
       <div>
         <Container style={{ minHeight: "80%" }}>
           <h1 className="text-center">Login</h1>
-        <LoginForm />
+          <LoginForm />
         </Container>
       </div>
     );
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,15 +1,58 @@
 import React, { Component } from "react";
+
+import Test from "test";
 import Container from "../components/Container";
 import SignupForm from "../components/SignupForm";
 
-class Signup extends Component {
+const Validator = require("validator");
+const isEmpty = require("is-empty");
 
+
+//Function to validate the signup
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+  // Convert empty fields to an empty string so we can use validator functions
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  // Name checks
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+  // Email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  // Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
+
+class Signup extends Component {
   render() {
     return (
       <div>
         <Container style={{ minHeight: "80%" }}>
           <h1 className="text-center">Signup</h1>
-        <SignupForm />
+          <SignupForm />
         </Container>
       </div>
     );
