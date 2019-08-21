63ad77e963de7b6cb11cb9bf7dfac811d5926156
Joyce Buettner <iamtechiegirl@cox.net>
2019-08-21 13:56:02 -0700

    FE-BE login-auth complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 // import Signup from "./components/auth/Register"; --JB coded out 906pm
@@ -11,6 +14,27 @@ import Wrapper from "./components/Wrapper";
 import API from "./utils/API";
 import { Provider } from "react-redux";
 import store from "./store";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/Dashboard/dashboard";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends Component {
 
@@ -46,6 +70,9 @@ class App extends Component {
             <Route exact path="/products" component={() => <Products info={this.state} />}/>
             <Route exact path="/signup" component={Signup} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
           </Wrapper>
           <Footer />
         </div>
