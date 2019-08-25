7fa6287df66a48641adfd26ceff5af5766618808
Mafalda <mafalda.villazan@gmail.com>
2019-08-24 18:57:43 -0700

    First commit for pull

---
+++ client/build/precache-manifest.e744ed74483e98ed1d22a18b4d077557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc168dd370b5c0e9fb36",
+    "url": "/static/css/main.72f3ac45.chunk.css"
+  },
+  {
+    "revision": "fc168dd370b5c0e9fb36",
+    "url": "/static/js/main.789639c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7843c01412a6653ed301",
+    "url": "/static/js/2.2653945e.chunk.js"
+  },
+  {
+    "revision": "5ea8a217be9fb6ee17b2adcb4b29a54c",
+    "url": "/static/media/LIEREN.5ea8a217.png"
+  },
+  {
+    "revision": "ca8650729fcc99a3d920c98a9725de73",
+    "url": "/static/media/Jumbo.ca865072.png"
+  },
+  {
+    "revision": "c8c0039d373992bdfd30918f52d633bc",
+    "url": "/index.html"
+  }
+];
